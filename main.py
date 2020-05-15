e06b2d9ef26df3657e205cfb50d62f143d576651
Fellipe <cliente@MacBook-Pro-de-BPO.local>
2020-05-15 19:03:56 -0300

    commit

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ while True:
             try:
                 numeros_inteiros = numeros_para_int()
                 print(f"O resultado da soma dos números fornecidos é: {round(c.soma(*numeros_inteiros), 2)}")
-                opcao2 = input(f"Fazer outra operação de soma? [S/N]").lower()
+                opcao2 = str(input(f"Fazer outra operação de soma? [S/N]")).lower()
+                while opcao2 not in "sn":
+                    print("opcão inválida")
+                    opcao2 = str(input(f"Fazer outra operação de soma? [S/N]")).lower()
                 if opcao2 == "n":
                     break
             except Exception as e:
@@ -25,6 +28,9 @@ while True:
                 numeros_inteiros = numeros_para_int()
                 print(f"O resultado da subtração dos números fornecidos é: {round(c.subtrai(*numeros_inteiros), 2)}")
                 opcao2 = input(f"Fazer outra operação de subtração? [S/N]").lower()
+                while opcao2 not in "sn":
+                    print("opcão inválida")
+                    opcao2 = str(input(f"Fazer outra operação de subtração? [S/N]")).lower()
                 if opcao2 == "n":
                     break
             except Exception as e:
@@ -33,7 +39,10 @@ while True:
             try:
                 numeros_inteiros = numeros_para_int()
                 print(f"O resultado da multiplicação dos números fornecidos é: {round(c.multiplica(*numeros_inteiros), 2)}")
-                opcao2 = input("Fazer outra operação de multiplicação?").lower()
+                opcao2 = input("Fazer outra operação de multiplicação? [S/N]").lower()
+                while opcao2 not in "sn":
+                    print("opcão inválida")
+                    opcao2 = str(input(f"Fazer outra operação de multiplicação? [S/N]")).lower()
                 if opcao2 == "n":
                     break
             except Exception as e:
@@ -42,7 +51,10 @@ while True:
             try:
                 numeros_inteiros = numeros_para_int()
                 print(f"O resultado da divisão dos números fornecidos é: {round(c.divide(*numeros_inteiros), 2)}")
-                opcao2 = input("Fazer outra operação de divisão?").lower()
+                opcao2 = input("Fazer outra operação de divisão? [S/N]").lower()
+                while opcao2 not in "sn":
+                    print("opcão inválida")
+                    opcao2 = str(input(f"Fazer outra operação de divisão? [S/N]")).lower()
                 if opcao2 == "n":
                     break
             except Exception as e:
