067a5ceb86e78dde7206a6f0590ee7f0f4c31d08
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2020-12-22 16:26:22 -0300

    feature(calculator): add new method 'operator' to orchestrator the call of arithmetics operations

--- src/calculator.py
+++ src/calculator.py
@@ -1,26 +1,42 @@
+from utils.python_utils import Utils as utils
+
 class Calculador:
+
+    def operator(self, operation):
+        method_name = operation
+        method = getattr(self, method_name, lambda: "Invalid")
+        if operation != "exit":
+            numeros_inteiros = utils.numeros_para_int()
+            return method(*numeros_inteiros)
+        return method()
+
+
+
     def soma(self, *args):
         res = 0
         for numbers in args:
             res += numbers
-        return res
+        return round(res, 2)
 
-    def subtrai(self, *args):
+    def subtracao(self, *args):
         res = args[0]
         for numbers in args[1:]:
             res -= numbers
-        return res
+        return round(res, 2)
 
-    def multiplica(self, *args):
+    def multiplicacao(self, *args):
         res = 1
         for numbers in args:
             res *= numbers
-        return res
+        return round(res, 2)
 
-    def divide(self, *args):
+    def divisao(self, *args):
         res = args[0]
         for numbers in args[1:]:
             if numbers == 0:
                 return "Not a number"
             res /= numbers
-        return res
+        return round(res, 2)
+
+    def exit(self):
+        quit()
