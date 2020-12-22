3b1a571e91a3e13462d44148d376ce6a4fc88df6
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2020-12-22 19:43:19 -0300

    fix(calculator): change return of division method

--- src/calculator.py
+++ src/calculator.py
@@ -3,14 +3,11 @@ from utils.python_utils import Utils as utils
 class Calculador:
 
     def operator(self, operation):
-        method_name = operation
-        method = getattr(self, method_name, lambda: "Invalid")
+        method = getattr(self, operation)
         if operation != "exit":
             numeros_inteiros = utils.numeros_para_int()
             return method(*numeros_inteiros)
         return method()
-
-
 
     def soma(self, *args):
         res = 0
@@ -34,7 +31,7 @@ class Calculador:
         res = args[0]
         for numbers in args[1:]:
             if numbers == 0:
-                return "Not a number"
+                return "Divisão por 0 não permitida!"
             res /= numbers
         return round(res, 2)
 
