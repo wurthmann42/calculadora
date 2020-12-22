b3e0475c4c4ec894e6b7c68f71a382a23e50e5cb
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2020-12-21 22:55:03 -0300

    feature(utils): add static method for utils and rename file

---
+++ utils/python_utils.py
@@ -0,0 +1,17 @@
+class Utils:
+
+    @staticmethod
+    def operacoes():
+        print("[1] SOMA\n" "[2] SUBTRAI\n" "[3] MULTIPLICA\n" "[4] DIVIDE\n" "[5] SAIR")
+
+    @staticmethod
+    def numeros_para_int():
+        numeros = input("Digite os números separados por vírgula: ")
+        numeros_splitado = numeros.split(",")
+        numbers = [float(x) if "." in x else int(x) for x in numeros_splitado]
+        return numbers
+
+    @staticmethod
+    def opcao():
+        opcao = int(input("Digite a sua opção:"))
+        return opcao
