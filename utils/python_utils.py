1c85df915931bdbd48a072b60a3d22ded49bf656
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2020-12-22 19:46:37 -0300

    fix(utils): add `try except` to numeros_para_int method

--- utils/python_utils.py
+++ utils/python_utils.py
@@ -1,13 +1,16 @@
 class Utils:
 
     @staticmethod
-    def operacoes():
+    def display_operations():
         print("[1] SOMA\n" "[2] SUBTRAI\n" "[3] MULTIPLICA\n" "[4] DIVIDE\n" "[5] SAIR")
 
     @staticmethod
     def numeros_para_int():
-        numeros = input("Digite os números separados por vírgula: ")
-        numeros_splitado = numeros.split(",")
-        numbers = [float(x) if "." in x else int(x) for x in numeros_splitado]
-        return numbers
+        try:
+            numeros = input("Digite os números separados por vírgula: ")
+            numeros_splitado = numeros.split(",")
+            numbers = [float(x) if "." in x else int(x) for x in numeros_splitado]
+            return numbers
+        except Exception as e:
+            print("Ops! Voce digitou errado... Digite novamente: ")
 
